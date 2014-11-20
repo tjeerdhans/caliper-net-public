53f341a2b9509b671406ca6257f8b76f274360ad
khammouda <khaled.hammouda@d2l.com>
2014-11-20 15:10:50 -0500

    added ShareAnnotation type

---
+++ src/ImsGlobal.Caliper/ImsGlobal.Caliper/Entities/Annotation/ShareAnnotation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace ImsGlobal.Caliper.Entities.Annotation {
+
+	public class ShareAnnotation : Annotation {
+
+        public ShareAnnotation(string id)
+			: base( id ) {
+			this.Type = AnnotationType.Shared.Uri;
+            this.WithAgents = new List<string>();
+		}
+
+		[JsonProperty( "withAgents", Order = 31 )]
+        public IList<string> WithAgents { get; set; }
+
+	}
+}
