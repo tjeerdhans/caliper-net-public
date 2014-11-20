215b58d79c929f0531ca807215f10f107b80b686
khammouda <khaled.hammouda@d2l.com>
2014-11-20 15:39:17 -0500

    changed share annotation to use IAgent instead of just IDs

--- src/ImsGlobal.Caliper/ImsGlobal.Caliper/Entities/Annotation/ShareAnnotation.cs
+++ src/ImsGlobal.Caliper/ImsGlobal.Caliper/Entities/Annotation/ShareAnnotation.cs
@@ -6,17 +6,18 @@ using System.Text;
 using Newtonsoft.Json;
 
 namespace ImsGlobal.Caliper.Entities.Annotation {
+    using ImsGlobal.Caliper.Entities.Foaf;
 
 	public class ShareAnnotation : Annotation {
 
         public ShareAnnotation(string id)
 			: base( id ) {
 			this.Type = AnnotationType.Shared.Uri;
-            this.WithAgents = new List<string>();
+            this.WithAgents = new List<IAgent>();
 		}
 
 		[JsonProperty( "withAgents", Order = 31 )]
-        public IList<string> WithAgents { get; set; }
+        public IList<IAgent> WithAgents { get; set; }
 
 	}
 }
